8f729cfe5b834f2a8ce01f5297fafeb359d5de91
Frank Liu <frankliu@Franks-MacBook-Air.local>
2016-04-14 21:26:07 -0700

    added star rating

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,16 @@
+// this kind of works but the stars went big
+// var setup_rating = function()
+// {
+//   $(".rating").rating();
+// }
+// $(document).ready(setup_rating)
+// $(document).on("page:load", setup_rating)
+
+// this doesn't work:
+// $(document).ready(
+// (function() {
+//   // initialize with defaults
+//   // $("#input-id").rating();
+//   //alert("hello")
+//   $(".rating").rating();
+// })
