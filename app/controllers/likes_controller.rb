950fbf14e0aa1ab0f0aa88d52b2906ece293876a
Frank Liu Mac <frankliu81@gmail.com>
2016-05-03 18:51:01 -0700

    AJAXify review like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,8 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
+  before_action :review # this will call the question method to force
+                          # finding a question as we will need it for both
+                          # the create and destroy actions
 
   def create
     like = Like.new
@@ -7,16 +10,31 @@ class LikesController < ApplicationController
     like.user = current_user
     like.review = review
     #byebug
-    if like.save
-      redirect_to product_path(review.product), notice: "Liked!"
-    else
-      redirect_to product_path(review.product), alert: "You have already liked!"
+
+    respond_to do |format|
+      if like.save
+        format.html { redirect_to product_path(review.product), notice: "Liked!" }
+        # render looks for which partial to be rendering, then the parameters
+        # the locals is necessary when you are rendering views, or
+        # you are rendering partial with the partial: key
+        format.js { render "create", locals: {review: review} } # likes/create.js.erb
+        # tried the below, but it won't work without locals, review will be nil
+        #format.js { render "create", review: review }
+      else
+        format.html { redirect_to product_path(review.product), alert: "You have already liked!" }
+        format.js { render js: "alert)'Can\'t like, please refresh the page!');" }
+      end
     end
   end
 
   def destroy
     like.destroy
-    redirect_to product_path(like.review.product), notice: "Unliked!"
+    respond_to do |format|
+      format.html { redirect_to product_path(like.review.product), notice: "Unliked!" }
+      format.js { render "destroy", locals: {review: review} }
+      # tried the below, but it won't work without locals, review will be nil
+      # format.js { render "destroy", review: review }
+    end
   end
 
   private
